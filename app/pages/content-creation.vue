b925487bfed3f5686f9d7acfad8d03e730cc1f71
Larryon Truman <lwtruman2@gmail.com>
2025-12-11 12:44:56 -0500

    add content creation

---
+++ app/pages/content-creation.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="prose mx-auto py-16">
+    <h1>Content Creation</h1>
+    <p>This page is coming soon. If you’re seeing this in production, it means Lar is still cooking up something good 🍳.</p>
+  </main>
+</template>
