019a844dc13776fd2de980953e53a426c1976d39
Larryon Truman <lwtruman2@gmail.com>
2025-12-19 15:43:30 -0500

    chore(layout): minor layout and style adjustments in default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,9 @@
-<template>
-  <div class="min-h-screen scroll-smooth bg-gradient-to-r from-primary-400/10 via-transparent to-primary-400/10 dark:from-transparent dark:via-primary-200/10 dark:to-transparent">
-    <AppHeader />
+<script setup lang="ts">
+</script>
 
+<template>
+  <div class="min-h-screen scroll-smooth bg-gradient-to-r from-primary-400/10 via-transparent to-primary-400/10 dark:from-transparent dark:via-primary-200/10 dark:from-transparent dark:via-primary-200/10 dark:to-transparent">
+    <AppHeader />
     <UMain>
       <slot />
     </UMain>
