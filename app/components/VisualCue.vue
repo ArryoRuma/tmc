197a96ea9bf39ce9affb5f618042894b1856637a
Larryon Truman <lwtruman2@gmail.com>
2025-12-22 11:00:44 -0500

    feat: add VisualCue component

---
+++ app/components/VisualCue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center items-center">
+    <UIcon name="i-lucide-arrow-down" class="w-6 h-6" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
