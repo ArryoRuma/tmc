80a93ca0bbf16a5d734c0192486af17dc4073075
Larryon Truman <lwtruman2@gmail.com>
2025-12-29 20:10:08 -0500

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -82,7 +82,7 @@ export default defineNuxtConfig({
   scripts: {
     registry: {
       googleAnalytics: {
-        id: ["G-G22P0WJNVM", "GTM-T6DBWPNS"],
+        id: "GTM-T6DBWPNS"
       },
     },
   },
