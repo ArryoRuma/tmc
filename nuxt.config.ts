4b72c75ba827acd9806ac4e4b09e03265c9ddc10
Larryon Truman <lwtruman2@gmail.com>
2025-12-30 16:05:08 -0500

    Remove Apollo tracking plugin and related runtime configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -85,11 +85,6 @@ export default defineNuxtConfig({
       },
     ],
   },
-  runtimeConfig: {
-    public: {
-      apolloTrackingId: "663123b733ed510438edeb4f",
-    },
-  },
   scripts: {
     registry: {
       googleAnalytics: {
